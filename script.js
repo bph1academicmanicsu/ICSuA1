f77e61d839b4e331cd1eb471178077a9121ebea9
bph1academicmanicsu <bph.1.academic.manicsu@gmail.com>
2026-08-28 10:21:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GANTI DENGAN URL WEB APP GOOGLE APPS SCRIPT ANDA (DEPLOYMENT EXECUTABLE URL)
-const GAS_URL = "https://script.google.com/macros/s/AKfycby1YIiSPrKKpjXQHqtldRFBvS3W_ukHHTkBwiEreYT6-Xc0aZqfxMHzSf5MjqweXNkUMw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzxl0jOCVSupUVed3c2PYuCMyF3SJ7fVUlFXP1FGeADkpmJqUEzTq8U6UujmEyxsNcR/exec";
         
 
 document.getElementById('careerForm').addEventListener('submit', async function(e) {
